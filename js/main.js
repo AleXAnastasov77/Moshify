d88bfa63060a9430a6e82db9f12bd95ab0a0aa77
AleXAnastasov77 <aleks13361@gmail.com>
2024-02-22 21:03:27 +0200

    Build the footer component

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
-// Get the SVG icon element
-const svgIcon = document.querySelector(".collapsible");
-
-// Get the collapsible div element
-const collapsibleDiv = document.querySelector(".collapsible");
-
-// Add a click event listener to the SVG icon
-svgIcon.addEventListener("click", function () {
-  // Toggle the "collapsible--expanded" class on the collapsible div
-  collapsibleDiv.classList.toggle("collapsible--expanded");
-});
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
